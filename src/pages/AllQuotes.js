6651ed3abca8196f8f2fe2e87203ec2beeb8a34a
Rob Horner <rdhjr81@gmail.com>
2022-11-20 15:21:55 -0500

    add highlighted  quote cmpt

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,16 @@
 import QuoteList from '../components/quotes/QuoteList'
 const DUMMY_QUOTES = [
     {
-        id:1,
-        author:'joe schmoe',
-        text: 'react is great'
+      id: '1',
+      author: "joe schmoe",
+      text: "react is great",
     },
     {
-        id:1,
-        author:'tom thom',
-        text: 'react is peachy'
-    }
-]
+      id: '2',
+      author: "tom thom",
+      text: "react is peachy",
+    },
+  ];
 const AllQuotes = () => {
     return (<QuoteList quotes={DUMMY_QUOTES}/>)
 };
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,33 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+
+const DUMMY_QUOTES = [
+  {
+    id: '1',
+    author: "joe schmoe",
+    text: "react is great",
+  },
+  {
+    id: '2',
+    author: "tom thom",
+    text: "react is peachy",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((q) => q.id === params.quoteId);
+  if(!quote){
+    return <p>No Quote Found</p>
+  }
   return (
     <Fragment>
-      <h1>Quotes Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
       <Route path={`/quotes/${params.quoteId}/comments`}>
-            <Comments></Comments>
+        <Comments></Comments>
       </Route>
     </Fragment>
   );
