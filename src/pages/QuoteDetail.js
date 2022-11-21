fb7770db24601a44737b6fe72b54108cbbc07ef5
Rob Horner <rdhjr81@gmail.com>
2022-11-21 10:56:37 -0500

    display 'load comments' btn only when comment cmpt is not displayed

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,17 +1,16 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link } from "react-router-dom";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
-
 const DUMMY_QUOTES = [
   {
-    id: '1',
+    id: "1",
     author: "joe schmoe",
     text: "react is great",
   },
   {
-    id: '2',
+    id: "2",
     author: "tom thom",
     text: "react is peachy",
   },
@@ -20,13 +19,23 @@ const DUMMY_QUOTES = [
 const QuoteDetail = () => {
   const params = useParams();
   const quote = DUMMY_QUOTES.find((q) => q.id === params.quoteId);
-  if(!quote){
-    return <p>No Quote Found</p>
+  if (!quote) {
+    return <p>No Quote Found</p>;
   }
+
+  const quotePath = `/quotes/${params.quoteId}`;
+  const quoteCommentPath = quotePath + "/comments";
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={quotePath} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={quoteCommentPath}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={quoteCommentPath}>
         <Comments></Comments>
       </Route>
     </Fragment>
