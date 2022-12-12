d1aac0553723a78097a0ffd04357800a93566224
Rob Horner <rdhjr81@gmail.com>
2022-12-12 09:55:48 -0500

    post new quotes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-http-e781c-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://react-http2-15f7f-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
